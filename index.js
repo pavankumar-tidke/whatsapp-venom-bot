1e85a6df6166b2e5a3bf2f4dff94205a73c1b449
userpavankumar <pavankumartidke12345@gmail.com>
2021-09-04 12:07:18 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Supports ES6
+// import { create, Whatsapp } from 'venom-bot';
+const venom = require('venom-bot');
+
+venom
+  .create()
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+
+
+function start(client) {
+  client.onMessage((message) => {
+    if (message.body != '' && message.isGroupMsg === false) {
+      client
+        .sendText(message.from, 'Hi, I am Pavan sir\'s robot assistant, please wait for reply... ')
+        .then((result) => {
+          console.log('Result: ', result); //return object success
+        })
+        .catch((erro) => {
+          console.error('Error when sending: ', erro); //return object error
+        });
+    }
+  });
+}
